a8805d0614c078b21b849ae12abcbf625482cd7e
Berkaycan Sakar <berkaycansakar@gmail.com>
2023-02-14 20:21:57 +0300

    Handling Errors

--- api/app.js
+++ api/app.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan'); // Http isteklerini kaydetmek için kullanılır.
 
 const productRoutes = require('./routes/products');
 const orderRoutes = require('./routes/orders');
 
+app.use(morgan('dev'));
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+});
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        error:{
+            message: err.message
+        }
+    })
+})
+
+
+
 
 module.exports = app;
