74ca6bbfaba9868f1b316b0a9d9e4e5dae9290fe
Berkaycan Sakar <berkaycansakar@gmail.com>
2023-02-17 23:21:46 +0300

    populating query with mongoose

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const order = require('../models/order');
 
 const Order = require('../models/order');
-const product = require('../models/product');
+
 
 router.get('/', (req, res, next) => {
     Order
         .find({})
-        .select('productId quantity')
+        .select('product quantity')
+        .populate('product',['name','price'])
         .exec()
         .then(doc => {
-            res.status(200).json({ 
-                count:doc.length,
-                orders:doc
-             });
+            res.status(200).json({
+                count: doc.length,
+                orders: doc
+            });
         })
         .catch(err => {
             res.status(500).json({ error: err });
@@ -25,13 +25,13 @@ router.get('/', (req, res, next) => {
 router.post('/', (req, res, next) => {
     const order = new Order({
         _id: mongoose.Types.ObjectId(),
-        productId: req.body.productId,
+        product: req.body.productId,
         quantity: req.body.quantity
     })
     order
         .save()
         .then(result => {
-            res.status(201).json({ message: 'Added order', result });
+            res.status(201).json({ message: 'Added order',result });
         })
         .catch(err => {
             res.status(500).json({ error: err });
@@ -43,10 +43,11 @@ router.get('/:orderId', (req, res, next) => {
     const id = req.params.orderId;
     Order
         .findById(id)
-        .select('productId quantity')
+        .select('product quantity')
+        .populate('product')
         .exec()
         .then(order => {
-            res.status(200).json({ order,id});
+            res.status(200).json(order);
         })
         .catch(err => {
             res.status(500).json({ error: err });
